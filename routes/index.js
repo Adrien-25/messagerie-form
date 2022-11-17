233dded8e64fc0c6cbe82b349e0d5772a0063540
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2022-11-17 18:30:20 +0100

    Mise en page des messages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {check, validationresult} = require('express-validator');
+const {check, validationResult} = require('express-validator');
 const router = express.Router();
 
 // const messageRoute = require('./message');
@@ -8,6 +8,30 @@ module.exports = (params) => {
 
     router.get('/', (requete, reponse) => {
         reponse.render('pages/index',{pageTitle: 'Messagerie form',message: requete.body});
+    });
+    router.post('/', [
+        check('title')
+        .trim()
+        .isLength({min:1})
+        .escape()
+        .withMessage('Le titre doit avoir au moins 1 caractères est requis'),
+        check('message')
+        .trim()
+        .isLength({min:5})
+        .withMessage('Le titre doit avoir au moins 5 caractères est requis'),
+        check('name')
+        .trim()
+        .isLength({min:3})
+        .escape()
+        .withMessage('Le nom doit avoir au moins 3 caractères est requis')
+    ],(requete, reponse) => {
+        const erreurs = validationResult(requete);
+
+        let messages = {};
+        if (!erreurs.isEmpty()){
+            messages = { erreurs:erreurs.array()};
+        }
+        reponse.render('pages/index',{pageTitle: 'Messagerie form',message: requete.body, messages:messages.erreurs});
     });
 
     router.use('/',(requete,reponse) => {
